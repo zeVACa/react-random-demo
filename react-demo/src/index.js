4f041a26ab291a3d76acc48389ba54ccd4c2939b
zeVACa <logunik@bk.ru>
2021-01-23 23:07:52 +0300

    card filtering

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import App from './components/App';
 
-import cardsDataResponse from './data.json';
-
-import Card from './card';
-
-console.log(cardsDataResponse);
-
-function CardList() {
-  return (
-    <div className="container">
-      <ul className="row">
-        {cardsDataResponse.map((item) => (
-          <Card imgUrl={item.image} title={item.title} description={item.description} />
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-ReactDOM.render(<CardList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
